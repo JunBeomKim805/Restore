9853b6f656fc8569c275481c314d499662dcfd29
JunbeomKim <kimjunbum00805@gmail.com>
2022-07-04 23:37:40 -0400

    publish to heroku

--- API/Startup.cs
+++ API/Startup.cs
@@ -58,8 +58,34 @@ namespace API
                     }
                 });
             });
-            services.AddDbContext<StoreContext>(opt=>{
-                opt.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<StoreContext>(options =>
+            {
+                var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+                string connStr;
+
+                if (env == "Development")
+                {
+                    connStr = Configuration.GetConnectionString("DefaultConnection");
+                }
+                else
+                {
+                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                    connUrl = connUrl.Replace("postgres://", string.Empty);
+                    var pgUserPass = connUrl.Split("@")[0];
+                    var pgHostPortDb = connUrl.Split("@")[1];
+                    var pgHostPort = pgHostPortDb.Split("/")[0];
+                    var pgDb = pgHostPortDb.Split("/")[1];
+                    var pgUser = pgUserPass.Split(":")[0];
+                    var pgPass = pgUserPass.Split(":")[1];
+                    var pgHost = pgHostPort.Split(":")[0];
+                    var pgPort = pgHostPort.Split(":")[1];
+
+                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb};SSL Mode=Require;Trust Server Certificate=true";
+                }
+
+                options.UseNpgsql(connStr);
             });
             services.AddCors();
             services.AddIdentityCore<User>(opt=>
